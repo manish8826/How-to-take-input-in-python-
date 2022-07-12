8524b738bba972efd33d8d63bbdd9f39c0f6b6fa
manish8826 <107915498+manish8826@users.noreply.github.com>
2022-07-12 13:27:17 +0530

    Add files via upload

---
+++ how_to_take_input.py
@@ -0,0 +1,12 @@
+name = input("what is your name ")
+ 
+print("hello "  + name)
+
+#adding process between hello and name is called concatination
+
+
+#Question- Tony is secretly a superhero ask him for his superhero name & show it to the terminal
+
+superhero_name = input("what is your superhero name?")
+print("wassup " + superhero_name)
+
